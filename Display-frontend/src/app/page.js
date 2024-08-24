09dce13360179569e4a8a9837cfc03b1ae0c0de2
Manas Kumar <141910018+manaskumar3003@users.noreply.github.com>
2024-08-24 21:41:36 +0530

    merge backend + frontend

--- Display-frontend/src/app/page.js
+++ Display-frontend/src/app/page.js
@@ -1,10 +1,13 @@
 import Navbar from "@/components/Navbar"
+import Detail from "@/components/section/landing/detail"
+import Hero from "@/components/section/landing/hero"
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <h2 className="text-3xl font-bold text-center p-5">Welcome To HomePge</h2>
+      <Hero/>
+      <Detail/>
     </>
   )
 }
