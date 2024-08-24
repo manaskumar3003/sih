386684cf3fa64612682f0f7cd25499543d47b62b
Kumar Piyush <147241876+Kumar2004-2009@users.noreply.github.com>
2024-08-24 08:52:02 -0700

    Update page.js

--- Display-frontend/src/app/page.js
+++ Display-frontend/src/app/page.js
@@ -4,8 +4,7 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      <h2 className="text-3xl font-bold text-center p-5">Welcome to Passport JS + JWT Authentication</h2>
-      <h3 className="text-2xl font-bold text-center p-5">Handle Access Token and Refresh Token on server</h3>
+      <h2 className="text-3xl font-bold text-center p-5">Welcome To HomePge</h2>
     </>
   )
 }
